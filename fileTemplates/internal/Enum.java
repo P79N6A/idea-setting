97c471ada98ba4bc135bbaaa17fef9f8b0f2f557
hjh <497804227@qq.com>
2019-01-26 00:38:27 +0800

    IU-2018.2.6 <Admin@DESKTOP-37MTGPI Create markdown.xml, IntelliLang.xml, project.default.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, yiiguxing.translation.xml, Default.xml, applicationLibraries.xml, Monokai Sublime Text 3.icls, file.template.settings.xml, ignore.xml, find.xml, keymap.xml, _@user_Material Oceanic.icls, _@user_Darcula.icls, github_settings.xml, mavenVersion.xml, Ladies Night 2.icls, Sublime Text 2.icls, Default.xml, scala.xml, debugger.renderers.xml, Eclipse copy.xml, editor.codeinsight.xml, jsp.jsp, editor.xml, debugger.xml, ide.general.xml, other.xml, git.xml, lombok-plugin.xml, other.xml, Custom.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, scala_config.xml, VisualVMLauncher.xml, databaseDrivers.xml, templates.xml, AnnotationType.java, laf.xml, KeyPromoterXStatistic.xml, parameter.hints.xml, Class.java, jdk.table.xml, path.macros.xml, vcs.xml, toStringTemplates.xml, mybatis.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ *${DESCRIPTION}
+ *
+ *@Author hjh
+ *@Date ${DATE}
+ **/
+public enum ${NAME} {
+}
